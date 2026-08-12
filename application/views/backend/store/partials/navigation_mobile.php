962ccd4b2ae4fdd69b65a42b6a6912bcc70e0776
Angel Tezo <angcorado4@gmail.com>
2026-08-11 18:24:54 -0600

    Eliminar archivo sensible

--- application/views/backend/store/partials/navigation_mobile.php
+++
@@ -1,7 +0,0 @@
-<div class="app-footer border-0 shadow-lg bg-primary-gradiant">
-    <a href="<?= base_url(); ?>portal/feed" class="nav-content-bttn nav-center"><i class="feather-home"></i></a>
-    <a href="<?= base_url(); ?>portal/stories" class="nav-content-bttn"><i class="feather-zap"></i></a>
-    <a href="<?= base_url(); ?>portal/travel_sales/<?= base64_encode(2); ?>" class="nav-content-bttn" data-tab="chats"><i class="feather-briefcase"></i></a>            
-    <a href="<?= base_url(); ?>portal/tasks" class="nav-content-bttn"><i class="feather-zap"></i></a>
-    <a href="<?= base_url(); ?>portal/user_profile/<?= base64_encode($this->session->userdata('login_user_id')); ?>" class="nav-content-bttn"><img style="border-radius:50%" src="<?php echo $this->crud_model->getPhoto('user',$this->session->userdata('login_user_id')); ?>" alt="user" class="w30 shadow-xss"></a>
-</div>
